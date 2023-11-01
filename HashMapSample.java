4450cdcc5dbe8029c430634434d93e652040f376
2jin2.1031 <2jin2.1031@gmail.com>
2023-11-01 19:03:00 +0900

    feat) 해쉬맵 사용법 익히기

--- HashMapSample.java
+++ HashMapSample.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class HashMapSample {
+import java.util.HashMap;
+
+public class HashMapSample {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("이진", "2");
+        map.put("정수미", "1");
+        System.out.println(map.get(2)); // "사람" 출력
+    }
 }
