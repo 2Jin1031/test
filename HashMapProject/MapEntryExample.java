129120ddd9afebaf6f6675b6eb6b7910a84c9081
2jin2.1031 <2jin2.1031@gmail.com>
2023-11-16 14:50:09 +0900

    feat: entry 사용법 익히기

--- HashMapProject/MapEntryExample.java
+++ HashMapProject/MapEntryExample.java
@@ -1,2 +1,19 @@
-package HashMapProject;public class MapEntryExample {
+package HashMapProject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapEntryExample {
+    public static void main(String[] args) {
+        // 예제 맵 생성
+        Map<String, Integer> orderMap = new HashMap<>();
+        orderMap.put("Pizza", 2);
+        orderMap.put("Burger", 1);
+        orderMap.put("Coke", 3);
+
+        // 맵의 각 엔트리를 출력
+        for (Map.Entry<String, Integer> entry : orderMap.entrySet()) {
+            System.out.println("Item: " + entry.getKey() + ", Quantity: " + entry.getValue());
+        }
+    }
 }
