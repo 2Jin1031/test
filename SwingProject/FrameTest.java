a2d8ac62d0aa737d73890b544da7c093eae33f44
2jin2.1031 <2jin2.1031@gmail.com>
2023-11-27 23:39:23 +0900

    feat: 프레임 만드는 법 익히기(Frame 상속 받아서)

--- SwingProject/FrameTest.java
+++ SwingProject/FrameTest.java
@@ -1,2 +1,18 @@
-package SwingProject;public class FrameTest {
+package SwingProject;
+
+import java.awt.*;
+
+public class FrameTest extends Frame {
+    public FrameTest(String str) {
+        super(str);
+        Panel panel1 = new Panel();
+        panel1.setBackground(Color.RED);
+        add(panel1);
+        setSize(300, 300);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new FrameTest("페널 테스트");
+    }
 }
