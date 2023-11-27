2460683061be6dc72d36779e2f666f406bfbef16
2jin2.1031 <2jin2.1031@gmail.com>
2023-11-27 23:42:19 +0900

    feat: 다이얼로그 만드는 법 익히기
    - Dialog
        - 메인 윈도우 외에 메시지를 출력하거나 사용자로부터 자료를 입력 받을 때 사용하는 컨테이너

--- SwingProject/ModelessDialog.java
+++ SwingProject/ModelessDialog.java
@@ -1,2 +1,24 @@
-package SwingProject;public class ModelessDialog {
+package SwingProject;
+
+import java.awt.*;
+
+public class ModelessDialog extends Frame {
+    public ModelessDialog() {
+        super("다이얼로그 테스트");
+        // 객체 생성
+        Dialog d = new Dialog(this, "모덜리스 다이얼로그");
+        // 위치와 크기 설정
+        setBounds(100, 100, 400, 400);
+        // 창을 화면에 보이게 함
+        setVisible(true);
+        d.setBounds(200, 200, 500, 500);
+        // 크기 설정
+        d.setSize(200, 200);
+        // 다이어로그를 화면에 보이게 함
+        d.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new ModelessDialog();
+    }
 }
