3ba982e2a04dfdbf35bbb1f4b8dd09532ee4fc60
2jin2.1031 <2jin2.1031@gmail.com>
2023-11-27 23:38:56 +0900

    feat: 프레임 만드는 법 익히기(메인 메서드 내에서)

--- SwingProject/FrameTest1.java
+++ SwingProject/FrameTest1.java
@@ -1,2 +1,12 @@
-package SwingProject;public class FrameTest1 {
+package SwingProject;
+
+import java.awt.*;
+
+public class FrameTest1 {
+    public static void main(String[] args) {
+        Frame f = new Frame();
+        f.setTitle("첫 번째 프레임입니다.");
+        f.setBounds(100, 100, 300, 300); // setBounds: 위치와 크기를 설정
+        f.setVisible(true); // setVisible(boolean b): 파라미터 b값에 응해, 이 컴퍼넌트를 표시하는지 비표시로 하는지 결정
+    }
 }
