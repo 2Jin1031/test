721d320f5bce6d1bb62234a540497c95b0807614
2jin2.1031 <2jin2.1031@gmail.com>
2023-11-27 23:34:30 +0900

    feat: 체크 박스 만드는 법 익히기

--- SwingProject/Checkbox1.java
+++ SwingProject/Checkbox1.java
@@ -1,2 +1,51 @@
-package SwingProject;public class Checkbox1 {
+package SwingProject;
+import java.awt.Checkbox;
+import java.awt.CheckboxGroup;
+import java.awt.Frame;
+import java.awt.Panel;
+
+// 버튼과 마친가지로 겹치지 않도록 조심
+public class Checkbox1 extends Frame
+{
+    // 레이블이 있는 생성자
+    public Checkbox1(String str)
+    {
+        super(str);
+        // 패널 생성
+        Panel p = new Panel();
+        // 치킨 체크 박스 체크 표시
+        Checkbox cbx1=new Checkbox("치킨", true);
+        // 피자 체크 박스 디폴트여도 true
+        Checkbox cbx2=new Checkbox("피자");
+        // 피자 체크 박스 디폴트여도 true
+        Checkbox cbx3=new Checkbox("햄버거");
+        // 패널에 생성
+        p.add(cbx1);
+        p.add(cbx2);
+        p.add(cbx3);
+
+        // 체크 박스 그룹 생성 -> 그룹 안에서 하나밖에 체킹 못함
+        CheckboxGroup group = new CheckboxGroup();
+        // 치킨 체크 박스 체크 표시
+        Checkbox cbx4=new Checkbox("치킨",group,true);
+        // 피자 체크 박스 체크 표시X
+        Checkbox cbx5=new Checkbox("피자",group,false);
+        // 햄버거 체크 박스 체크 표시X
+        Checkbox cbx6=new Checkbox("햄버거",group,false);
+        // 패널에 생성
+        p.add(cbx4);
+        p.add(cbx5);
+        p.add(cbx6);
+
+        add(p);
+        // 윈도우 사이즈 180 x 300
+        setSize(180, 300);
+        // 윈도우
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Checkbox1("체크 박스 테스트");
+    }
+
 }
