508a8d6ceb899dfbf2a0c39e684bd2d298e9a0a1
2jin2.1031 <2jin2.1031@gmail.com>
2023-11-27 21:42:17 +0900

    feat: FlowLayout 배치관리자 활용

--- SwingProject/FlowLayoutEx.java
+++ SwingProject/FlowLayoutEx.java
@@ -11,7 +11,7 @@ public class FlowLayoutEx extends JFrame {
 
         // 수평 간격이 30, 수직 간격이 40 픽셀로 하고 LEFT로 배치함
         c.setLayout(new FlowLayout(FlowLayout.LEFT, 30, 40));
-        
+
         c.add(new JButton("add"));
         c.add(new JButton("sub"));
         c.add(new JButton("mul"));
